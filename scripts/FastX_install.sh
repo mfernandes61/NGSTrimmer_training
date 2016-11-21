0dda6721cbd0c892976b4305f0a7fa17f7793e29
Mark Fernandes <mfernandes61@gmail.com>
2016-11-21 16:08:50 +0000

    Create FastX_install.sh

---
+++ scripts/FastX_install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# set -e
+# Fastx-Toolkit Installation:
+
+# Install libgtextutils
+ wget http://cancan.cshl.edu/labmembers/gordon/files/libgtextutils-0.6.tar.bz2
+ tar -xjf libgtextutils-0.6.tar.bz2
+ cd libgtextutils-0.6
+ ./configure
+ make
+ sudo make install
+ cd ..
+ 
+ # Install fastx-toolkit
+ wget http://cancan.cshl.edu/labmembers/gordon/files/fastx_toolkit-0.0.12.tar.bz2 
+ tar -xjf fastx_toolkit-0.0.12.tar.bz2 
+ cd fastx_toolkit-0.0.12
+ ./configure
+ make
+ sudo make install
+ cd ..
+ 
+# update shared library cache
+sudo ldconfig
