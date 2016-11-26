74f14d38a7858386bcf310bb7de8c3e666387674
Mark Fernandes <mfernandes61@gmail.com>
2016-11-26 17:57:52 +0000

    Create prinseq_lite.sh

---
+++ scripts/prinseq_lite.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+# prinseq
+cd /tmp
+wget http://downloads.sourceforge.net/project/prinseq/standalone/prinseq-lite-0.20.4.tar.gz \
+-O /tmp/prinseq-lite-0.20.4.tar.gz
+tar -xvf /tmp/prinseq-lite-0.20.4.tar.gz
+sudo install -p tmp/prinseq-lite-0.20.4/prinseq-lite.pl /usr/local/bin
