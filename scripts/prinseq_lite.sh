bac9033e380259fc74c78a4c957e1d5917c3053a
Mark Fernandes <mfernandes61@gmail.com>
2016-12-05 15:24:10 +0000

    Update prinseq_lite.sh
    Adapt script to use /tools rather than /tmp

--- scripts/prinseq_lite.sh
+++ scripts/prinseq_lite.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 # prinseq
-cd /tmp
+cd /tools
 wget http://downloads.sourceforge.net/project/prinseq/standalone/prinseq-lite-0.20.4.tar.gz \
--O /tmp/prinseq-lite-0.20.4.tar.gz
-tar -xvf /tmp/prinseq-lite-0.20.4.tar.gz
-install -p tmp/prinseq-lite-0.20.4/prinseq-lite.pl /usr/local/bin
+-O /tools/prinseq-lite-0.20.4.tar.gz
+tar -xvf /tools/prinseq-lite-0.20.4.tar.gz
+rm /tools/prinseq-lite*.tar.gz
+install -p /tools/prinseq-lite-0.20.4/prinseq-lite.pl /usr/local/bin
