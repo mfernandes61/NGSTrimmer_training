316d8cd645d1211a94cb2043d3cdff0b6b604c1a
Mark Fernandes <mfernandes61@gmail.com>
2016-12-05 14:46:42 +0000

    Update prinseq_lite.sh
    Remove sudo from script (already root)

--- scripts/prinseq_lite.sh
+++ scripts/prinseq_lite.sh
@@ -4,4 +4,4 @@ cd /tmp
 wget http://downloads.sourceforge.net/project/prinseq/standalone/prinseq-lite-0.20.4.tar.gz \
 -O /tmp/prinseq-lite-0.20.4.tar.gz
 tar -xvf /tmp/prinseq-lite-0.20.4.tar.gz
-sudo install -p tmp/prinseq-lite-0.20.4/prinseq-lite.pl /usr/local/bin
+install -p tmp/prinseq-lite-0.20.4/prinseq-lite.pl /usr/local/bin
